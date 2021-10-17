2e2d8e098964bfc4c0f0e02b1fae88e080630cb2
Bruno Olivares <brunoos@al.insper.edu.br>
2021-10-17 20:26:51 -0300

    O Inicio do Jogo

---
+++ iniciojogo.py
@@ -0,0 +1,12 @@
+def inicio_de_jogo(numero_jogadores):
+
+    mão_jogadores = [[]]
+    i = 1
+    while i < numero_jogadores:
+        mão_jogadores.append([])
+        i += 1
+    return mão_jogadores
+
+
+
+
