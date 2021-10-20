600716f9cade2168252d0b31b82eb47c9e2b0261
Bruno Olivares <brunoos@al.insper.edu.br>
2021-10-19 22:13:30 -0300

    Segundo dia de trabalho

---
+++ código_principal.py
@@ -0,0 +1,37 @@
+from iniciojogo import inicio_de_jogo
+import random
+
+print("Seja Bem vindo ao nosso JOGO DE DOMINÓ")
+print("----------------------------------------------------------------------------------------------------------")
+print("Regras:")
+print("----------------------------------------------------------------------------------------------------------")
+
+q_jogadores = int(input("Entre quantos jogadores se dará este duelo (2 - 4)? "))
+
+while q_jogadores > 4 or q_jogadores < 2:
+    print("Não se pode jogar com essa quantidade de jogadores. Tente inserir outro número")
+    q_jogadores = int(input("Entre quantos jogadores se dará este duelo (2 - 4)? "))
+
+começo = inicio_de_jogo(q_jogadores)
+
+o_bolinho = []
+contador = 1
+
+if q_jogadores == 2:
+    while contador <= 14:
+        o_bolinho.append([str(random.randint(1,6)) + "|" + str(random.randint(1,6))])
+    
+elif q_jogadores == 3:
+    while contador <= 7:
+        o_bolinho.append([str(random.randint(1,6)) + "|" + str(random.randint(1,6))])
+
+else:
+    o_bolinho = []
+
+
+
+
+
+
+
+
--- peças_iniciais.py
+++ peças_iniciais.py
@@ -6,7 +6,7 @@ q_jogadores = int(input("Serão quantos jogadores na partida? "))
 os_jogadores = inicio_de_jogo(q_jogadores)
 
 for jogadores in os_jogadores:
-    for peças in range(0, 6):
+    for peças in range(0, 7):
         jogadores.append([str(random.randint(1 , 6)) + "|" + str(random.randint(1,6))])
 
 print (os_jogadores)
